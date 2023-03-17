3d8b0c1a552a4a1fd0c828ccd5b4f45d56d48abf
shreekrishnalamichhane <uniksk123@gmail.com>
2023-03-17 15:15:14 +0545

    V1.9 : UI Change

--- src/utils/all.js
+++ src/utils/all.js
@@ -2,8 +2,10 @@
 export const getFormattedDate = (date) =>
   date
     ? new Date(date).toLocaleDateString("en-us", {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      })
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    })
     : "";
+
+export const ucFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1);
