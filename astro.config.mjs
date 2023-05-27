d2c08be8c7226916fccbb70a7377cf642b15457b
shreekrishnalamichhane <uniksk123@gmail.com>
2023-05-27 15:26:28 +0545

    V1.16 : MathJax Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,22 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from "remark-math"
+import rehypeMathjax from "rehype-mathjax"
 import compress from "astro-compress";
 import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
   site: process.env.SITE_URL,
+  markdown: {
+    remarkPlugins: [
+      remarkMath
+    ],
+    rehypePlugins: [
+      rehypeMathjax
+    ]
+  },
   integrations: [
     tailwind(),
     image({
